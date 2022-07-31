af0ae7d3324b36b54b39d197b156181894e90d37
95Dongguri <hyuk_ey@naver.com>
2022-07-31 20:34:09 +0900

    Done

--- eazel-ios-assignmentTests/Tests/SettingViewModelTests.swift
+++ eazel-ios-assignmentTests/Tests/SettingViewModelTests.swift
@@ -1,34 +1,36 @@
-////
-////  SettingViewModelTests.swift
-////  eazel-ios-assignmentTests
-////
-////  Created by 김동혁 on 2022/07/29.
-////
 //
-//import FirebaseAuth
-//import XCTest
+//  SettingViewModelTests.swift
+//  eazel-ios-assignmentTests
 //
-//@testable import eazel_ios_assignment
+//  Created by 김동혁 on 2022/07/31.
 //
-//class SettingViewModelTests: XCTestCase {
-//    var sut: SettingViewModel!
-//
-//    override func setUp() {
-//        super.setUp()
-//
-//    }
-//
-//    override func tearDown() {
-//
-//        super.tearDown()
-//    }
-//
-//    func test_isEqaulUser() {
-//        let user = SettingViewModel(user: "user")
-//        
-//        XCTAssertTrue(user.getUser)
-//    }
-//}
-//
-//
-//
+
+import FirebaseAuth
+import XCTest
+@testable import eazel_ios_assignment
+
+class SettingViewModelTests: XCTestCase {
+    var sut: SettingViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut = SettingViewModel()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        
+        super.tearDown()
+    }
+    
+    func test_logout_Success() {
+        XCTAssertNoThrow(sut.logout(UIViewController()))
+    }
+    
+    func test_updateUser() {
+        sut.updateUser(UILabel())
+        
+        XCTAssertNil(UILabel().text)
+    }
+}
